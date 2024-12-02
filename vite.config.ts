442cb5efd7d66b9bb3ccb0c8c0262393988ce875
欧阳智勇 <ouyangzhiyong0415@qq.com>
2024-12-02 14:36:30 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: true
+  },
+
+  server: {
+    host: 'localhost',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://remote-dev-test.ouyangzhiyong0415.workers.dev',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
